8c0a1793d1a40bc59d3df21ad00f1eeb6235596e
Joseph Njenga <joseph.njenga@student.moringaschool.com>
2023-01-09 19:37:42 +0300

    VB-4 added login form

--- vilego-vlogs/src/components/Login.jsx
+++ vilego-vlogs/src/components/Login.jsx
@@ -9,12 +9,21 @@ function Login() {
           <input type="text" placeholder="First Name" />
           <input type="text" placeholder="Last Name" />
           <input type="text" placeholder="Username" />
-          <input type="email" placeholder="First Name" />
+          <input type="email" placeholder="Email" />
           <input type="password" placeholder="Password" />
           <input type="password" placeholder="Password Confirmation" />
+          <button>Sign Up</button>
         </form>
       </div>
-      <div className="login-container"></div>
+      <div className="login-container">
+        <form>
+          <h3>Sign In</h3>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <p>Forgot your password?</p>
+          <button>Login</button>
+        </form>
+      </div>
       <div className="overlay-container">
         <div className="left-overlay-panel"></div>
         <div className="right-overlay-panel"></div>
