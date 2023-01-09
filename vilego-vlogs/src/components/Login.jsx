f3f8481f7898a0d450579bdc166d4b85ccc21324
Joseph Njenga <joseph.njenga@student.moringaschool.com>
2023-01-09 19:22:06 +0300

    VB-4 added containers

---
+++ vilego-vlogs/src/components/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className="main-login-page-container">
+      <div className="signup-container"></div>
+      <div className="login-container"></div>
+      <div className="overlay-container">
+        <div className="left-overlay-panel"></div>
+        <div className="right-overlay-panel"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Login
