29758aea36ade0c5493cc9d7af58d756901be2b0
Joseph Njenga <joseph.njenga@student.moringaschool.com>
2023-01-09 19:34:37 +0300

    VB-4 added signup form

--- vilego-vlogs/src/components/Login.jsx
+++ vilego-vlogs/src/components/Login.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 function Login() {
   return (
     <div className="main-login-page-container">
-      <div className="signup-container"></div>
+      <div className="signup-container">
+        <form>
+          <h3>Create Account</h3>
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+          <input type="text" placeholder="Username" />
+          <input type="email" placeholder="First Name" />
+          <input type="password" placeholder="Password" />
+          <input type="password" placeholder="Password Confirmation" />
+        </form>
+      </div>
       <div className="login-container"></div>
       <div className="overlay-container">
         <div className="left-overlay-panel"></div>
