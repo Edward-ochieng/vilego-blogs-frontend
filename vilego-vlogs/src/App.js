0c57c7421d878218c37d312b5a8a3121f545eb6e
Blessed <blessedallan1@gmail>
2023-01-10 15:03:47 +0300

    VB-16 routes added

--- vilego-vlogs/src/App.js
+++ vilego-vlogs/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import react from 'react';
+import ReactDOM from'react-dom';
+import Article from './components/Article'
+import Login from './components/Login'
+import Signup from './components/Signup'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/article" element={<Article/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+      </Routes>
+        </BrowserRouter>
+  )
+    
+
 }
 
 export default App;
---
+++ vilego-vlogs/src/components/Signup.jsx
@@ -0,0 +1,13 @@
+import "../styles/Signup.css";
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div className="formInput">
+      <label>Username</label>
+      <input></input>
+    </div>
+  )
+}
+
+export default Signup
