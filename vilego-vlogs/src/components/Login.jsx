29b09736a3129915fc02c15aa5280a1ec7b603f8
Joseph Njenga <joseph.njenga@student.moringaschool.com>
2023-01-10 12:26:46 +0300

    VB-4 added inline styling

--- vilego-vlogs/src/components/Login.jsx
+++ vilego-vlogs/src/components/Login.jsx
@@ -1,12 +1,21 @@
-import React from "react";
-import "./Login.css";
+import React, { useState } from "react";
+import "./App.css";
 
-function Login() {
+function App() {
+  const [signIn, setSignin] = useState(true);
+
   return (
     <div className="main-login-page-container">
-      <div className="signup-container">
+      <div
+        className="signup-container"
+        style={{
+          transform: !signIn ? "translateX(100%)" : "",
+          opacity: !signIn ? 1 : 0,
+          zIndex: !signIn ? 5 : 1,
+        }}
+      >
         <form>
-          <h3>Create Account</h3>
+          <h2>Create Account</h2>
           <input type="text" placeholder="First Name" />
           <input type="text" placeholder="Last Name" />
           <input type="text" placeholder="Username" />
@@ -16,28 +25,57 @@ function Login() {
           <button>Sign Up</button>
         </form>
       </div>
-      <div className="login-container">
+      <div
+        className="login-container"
+        style={{
+          transform: !signIn ? "translateX(100%)" : "",
+        }}
+      >
         <form>
-          <h3>Sign In</h3>
+          <h2>Sign In</h2>
           <input type="email" placeholder="Email" />
           <input type="password" placeholder="Password" />
           <a href="#">Forgot password?</a>
           <button>Login</button>
         </form>
       </div>
-      <div className="overlay-container">
-        <div className="overlay">
-          <div className="overlay-panel left-overlay-panel">
-            <h3>Welcome Back!</h3>
+      <div
+        className="overlay-container"
+        style={{
+          transform: !signIn ? "translateX(-100%)" : "",
+        }}
+      >
+        <div
+          className="overlay"
+          style={{
+            transform: !signIn ? "translateX(50%)" : "",
+          }}
+        >
+          <div
+            className="overlay-panel left-overlay-panel"
+            style={{
+              transform: !signIn ? "translateX(0)" : "",
+            }}
+          >
+            <h2>Welcome Back!</h2>
             <p>
               To keep connected with us please login with your personal info
             </p>
-            <button className="overlay-button">Login</button>
+            <button className="overlay-button" onClick={() => setSignin(true)}>
+              Login
+            </button>
           </div>
-          <div className="overlay-panel right-overlay-panel">
-            <h3>Hello, Friend!</h3>
+          <div
+            className="overlay-panel right-overlay-panel"
+            style={{
+              transform: !signIn ? "translateX(20%)" : "",
+            }}
+          >
+            <h2>Hello, Friend!</h2>
             <p>Enter your personal details and start your journey with us</p>
-            <button className="overlay-button">Sign Up</button>
+            <button className="overlay-button" onClick={() => setSignin(false)}>
+              Sign Up
+            </button>
           </div>
         </div>
       </div>
@@ -45,4 +83,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default App;
