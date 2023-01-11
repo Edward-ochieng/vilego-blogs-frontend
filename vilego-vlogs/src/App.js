91573db911fa9c2de21f13e76a5835e8dc43729d
Blessed <blessedallan1@gmail>
2023-01-11 11:54:34 +0300

    VB-22 corrected the routes and included tailwind css configuration

--- vilego-vlogs/src/App.js
+++ vilego-vlogs/src/App.js
@@ -1,26 +1,20 @@
-import react from 'react';
-import ReactDOM from'react-dom';
-import Article from './components/Article'
+import Blog from './components/Blog'
 import Login from './components/Login'
 import Signup from './components/Signup'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/article" element={<Article/>}/>
+        <Route path="/blog" element={<Blog/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/signup" element={<Signup/>}/>
       </Routes>
         </BrowserRouter>
   )
-    
-
 }
-
-export default App;
