5d03bccffb3c20bc975256810ded40d6f649da3e
Edward <edward.ochieng@student.moringaschool.com>
2023-01-12 15:34:13 +0300

    reverted back to changes before this week

--- vilego-vlogs/src/App.js
+++ vilego-vlogs/src/App.js
@@ -1,20 +1,25 @@
-import Blog from './components/Blog'
-import Login from './components/Login'
-import Signup from './components/Signup'
-import Home from './components/Home';
-import Navbar from './components/Navbar';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
 
-export default function App() {
+function App() {
   return (
-    <BrowserRouter>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/blog" element={<Blog/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/signup" element={<Signup/>}/>
-      </Routes>
-        </BrowserRouter>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
